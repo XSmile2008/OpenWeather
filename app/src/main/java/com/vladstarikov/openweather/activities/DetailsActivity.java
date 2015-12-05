855baeb93190119198e3222abe452262c5198b2f
XSmile2008 <XSmile2008@gmail.com>
2015-12-05 16:10:30 +0200

    add DetailsActivity

---
+++ app/src/main/java/com/vladstarikov/openweather/activities/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.vladstarikov.openweather.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Starikov on 05.12.15.
+ */
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.);
+    }
+}
