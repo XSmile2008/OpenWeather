686cc1025378526a701cd758c1142bc781b4d81b
XSmile2008 <XSmile2008@gmail.com>
2015-12-21 19:00:03 +0200

    just commit

---
+++ app/src/main/java/com/vladstarikov/openweather/weather/realm/City.java
@@ -0,0 +1,11 @@
+package com.vladstarikov.openweather.weather.realm;
+
+import java.io.Serializable;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Starikov on 17.12.15.
+ */
+public class City extends RealmObject implements Serializable {
+}
